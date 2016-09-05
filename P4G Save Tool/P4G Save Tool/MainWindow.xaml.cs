f9d5b0ddb5c283d65002440d5e8a8b87a5d4f6ee
DuIslingr <alcatraz0890@live.com>
2016-09-04 20:15:11 -0400

    Various Edits. Removed Files

--- P4G Save Tool/P4G Save Tool/MainWindow.xaml.cs
+++ P4G Save Tool/P4G Save Tool/MainWindow.xaml.cs
@@ -2,22 +2,14 @@
 using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 
 namespace P4G_Save_Tool
